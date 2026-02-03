11ff132a0f9165ed8f374dc9f5291f336333b6e9
fffllyyy <149820019+fffllyyy@users.noreply.github.com>
2026-02-03 23:48:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
-const namaTarget = urlParams.get('Citra') || 'Citra';
+const namaTarget = urlParams.get('name') || 'Citra';
 
 // Update teks HTML
 document.getElementById('question-text').innerText = `${namaTarget}, will you be my valentine?`;
@@ -102,5 +102,6 @@ function createHeart() {
     document.body.appendChild(heart);
     setTimeout(() => { heart.remove(); }, 5000);
 }
+
 
 
